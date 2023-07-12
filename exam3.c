66c64c01768ac27cee29a57288e06064e374888b
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-07-12 16:43:05 +0530

    Add files via upload

--- exam3.c
+++ exam3.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 main()
 {
-	int fd,ld,sum,n;
+	int fd,ld,sum=0,n;
 	printf("enter number n=");
 	scanf("%d",&n);
 	ld=n%10;
-	fd=n/10;
-	while(n>0)
+	
+	while(n>=10)
 	{
-		ld=n%10;
-		fd=n/10;
+		
+		n=n/10;
+		
 	}
+	fd=n;
 	sum=ld+fd;
 	printf("sum=%d",sum);
 }
